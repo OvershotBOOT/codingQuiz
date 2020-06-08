f2259f5c7625ae42d6a168afc3eecf4da1fb8dc9
OvershotBOOT <tomseymour217@gmail.com>
2020-06-08 14:30:55 -0700

    Added function to check value of selected qa answer

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var startButtonEl
 var startScreenEl
 var qaBlocks
+var answers1
+var correctAnswer1
+var answers2
+var answers3
+var answers4
+var answers5
+var answers6
+var currentAnswer
 
 startButtonEl = document.getElementById('startButton');
 startScreenEl = document.getElementById('startScreen');
@@ -12,3 +20,25 @@ startButtonEl.addEventListener('click', function() {
 qaContainer = document.querySelectorAll('.qaContainer')
 
 console.log(qaContainer)
+
+answers1 = document.querySelectorAll('.buttonAnswer1')
+
+console.log(answers1)
+
+correctAnswer1 = answers1[0].innerHTML
+
+console.log(correctAnswer1)
+
+function bigBoy (inputArr) {
+    for (var arrValue of inputArr) {
+        arrValue.addEventListener('click', function() {
+            console.log(this.innerHTML)
+        })
+    }
+}
+
+bigBoy (answers1)
+
+
+
+
